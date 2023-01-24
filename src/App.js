105912f5f898d80cb89dee2c2ca60e8f3ede9972
Jym-ship-it <fjymcyruz@gmail.com>
2023-01-25 00:48:22 +0800

    disabled zoom in mobile view

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </header>
       <NavBar></NavBar>
       <Hero></Hero>
