27377ed5a589c2c8e8be10e97365c8044751512a
Jym-ship-it <fjymcyruz@gmail.com>
2023-01-24 16:46:29 +0800

    Update navBar.js

--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -62,7 +62,7 @@ export default function DrawerAppBar(props) {
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, color : "black"}}
           >
             Edie
           </Typography>
